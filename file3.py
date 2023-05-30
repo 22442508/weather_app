33cfd5f70fa2e9b639f299a77dc68dd43c087689
22442508 <126608466+22442508@users.noreply.github.com>
2023-05-30 15:29:36 +0500

    create file3.py

--- file3.py
+++ file3.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import requests
 
-def get_weather_data(city):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=YOUR_API_KEY&units=metric"
+def get_weather_data(city, api_key):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     data = response.json()
     return data
@@ -11,10 +11,11 @@ def main():
     st.title("Weather App")
     st.markdown("Check the weather for your location")
 
+    api_key = "YOUR_API_KEY"
     city = st.text_input("Enter your city")
     if st.button("Get Weather"):
         if city:
-            weather_data = get_weather_data(city)
+            weather_data = get_weather_data(city, api_key)
             if weather_data["cod"] == "404":
                 st.error("City not found. Please enter a valid city name.")
             else:
@@ -30,3 +31,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
